59253c520a9a7f5cbf08540f69e40e64dc4887e1
loyshang <80870728+loyshang@users.noreply.github.com>
2021-04-08 11:30:07 +0800

    Update eslint_check.sh

--- eslint_check.sh
+++ eslint_check.sh
@@ -5,5 +5,8 @@ cd ari
 echo $(pwd)
 docker run -i --rm -v $(pwd):/data cytopia/eslint "src/**" --ignore-pattern 'assets' --ignore-pattern 'vendors' -o eslint-ari-error.log --quiet
 echo "pass point 1"
-grep "errors" eslint-ari-error.log >> test.txt
-awk '{gsub(/[a-zA-Z]/, ""); print}' test.txt | awk '{split($0,a,"[(,]"); print a[2]; if(a[2]>$error_limit){exit 1}}'
+grep "errors" eslint-ari-error.log | awk '{gsub(/[a-zA-Z]/, ""); print}' | awk '{split($0,a,"[(,]"); print a[2]; if(a[2]>$error_limit){exit 1}}'
+
+
+#grep "errors" eslint-ari-error.log >> eslint-ari-error.txt
+#awk '{gsub(/[a-zA-Z]/, ""); print}' eslint-ari-error.txt | awk '{split($0,a,"[(,]"); print a[2]; if(a[2]>$error_limit){exit 1}}'
