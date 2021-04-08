9797ab2c4a3e2faf3c72a793499018368ce9123c
loyshang <80870728+loyshang@users.noreply.github.com>
2021-04-08 10:39:50 +0800

    Update eslint_check.sh

--- eslint_check.sh
+++ eslint_check.sh
@@ -2,7 +2,6 @@
 echo $(pwd)
 cd ../ari
 echo $(pwd)
-docker run -it --rm -v $(pwd):/data cytopia/eslint "/src/**" --ignore-pattern 'assets' --ignore-pattern 'vendors' -o error.log --quiet
 echo "pass point 1"
 grep "errors" error.log >> test.txt
 cat test.txt
